2bda5c49baedaf0e080c2f59692aaa18cee8f1ef
Assassin0795 <tonycarmalitano@yahoo.com>
2015-01-16 14:24:48 -0500

    Added tracking/saving for per-layer transparency.

--- LevEditor/src/HUD/Button/LayerWindowButton.cs
+++ LevEditor/src/HUD/Button/LayerWindowButton.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,25 @@ namespace LevEditor
     {
         private Texture2D texture;
         private float transparency;
+        private int sliderValue;
+
+        public int SliderValue
+        {
+            get { return sliderValue; }
+            set
+            {
+                sliderValue = value;
+
+                /* We start off with 100.0f as 100.0f / 100.0f = 1, which is a fully opaque layer mask.
+                 * We subtract 12 from sliderValue as 12 is the hardcoded position that the slider can
+                 * go furthest left to (creating an artificial "zero" value). We divide by 2 as the
+                 * slider button can traverse 200 pixels. Divide by 100 to get 0 <= value <= 1.
+                 * 
+                 * e.g. If the slider is at 140px: (100 - ((140 - 12) / 2)) / 100 = .36; closer to an invisible mask.
+                 */
+                transparency = (100.0f - ((sliderValue - 12) / 2)) / 100.0f;
+            }
+        }
 
         public float Transparency
         {
@@ -23,7 +43,8 @@ namespace LevEditor
             get { return texture; }
         }
 
-        public LayerWindowButton(Rectangle rectangle, Texture2D texture, int id) : base(rectangle, id)
+        public LayerWindowButton(Rectangle rectangle, Texture2D texture, int id)
+            : base(rectangle, id)
         {
             this.texture = texture;
             transparency = 1;
