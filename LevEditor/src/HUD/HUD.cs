ba2ff5054310e9a65380c68dd7f94a7a09293d55
Assassin0795 <tonycarmalitano@yahoo.com>
2015-01-15 20:16:35 -0500

    Removed TileEditWindow; replaced it with ToolsWindow.

--- LevEditor/src/HUD/HUD.cs
+++ LevEditor/src/HUD/HUD.cs
@@ -13,27 +13,24 @@ namespace LevEditor
     class HUD
     {
         private AssetsWindow assetsWindow;
-        private ToolsWindow ToolsWindow;
         private FileWindow fileWindow;
         private LayerEditWindow layerEditWindow;
-        private TileEditWindow tileEditWindow;
+        private ToolsWindow toolsWindow;
 
         public HUD()
         {
             fileWindow = new FileWindow(new Vector2(896, 0), new Vector2(256, 256));
             assetsWindow = new AssetsWindow(new Vector2(0, 256), new Vector2(256, 512));
-            tileEditWindow = new TileEditWindow(new Vector2(896, 256), new Vector2(256, 512));
+            toolsWindow = new ToolsWindow(new Vector2(896, 256), new Vector2(256, 512));
             layerEditWindow = new LayerEditWindow(Vector2.Zero, new Vector2(256, 256));
-            ToolsWindow = new ToolsWindow(new Vector2(256, 0), new Vector2(640, 192));
         }
 
         public bool Contains(Vector2 position)
         {
             if (fileWindow.BackgroundRectangle.Contains(position) ||
                 assetsWindow.BackgroundRectangle.Contains(position) ||
-                tileEditWindow.BackgroundRectangle.Contains(position) ||
                 layerEditWindow.BackgroundRectangle.Contains(position) ||
-                ToolsWindow.BackgroundRectangle.Contains(position))
+                toolsWindow.BackgroundRectangle.Contains(position))
                 return true;
 
             return false;
@@ -43,18 +40,16 @@ namespace LevEditor
         {
             fileWindow.Load(content, "Test_Content/BackgroundFileIOW.png");
             assetsWindow.Load(content, "Test_Content/BackgroundAssetsW.png");
-            tileEditWindow.Load(content, "Test_Content/BackgroundTileEditW.png");
+            toolsWindow.Load(content, "Test_Content/BackgroundTileEditW.png");
             layerEditWindow.Load(content, "Test_Content/BackgroundLayerEditW.png");
-            ToolsWindow.Load(content, "Test_Content/BackgroundEntityEditW.png");
         }
 
         public void Update(GameTime gameTime)
         {
             fileWindow.Update(gameTime);
             assetsWindow.Update(gameTime);
-            tileEditWindow.Update(gameTime);
+            toolsWindow.Update(gameTime);
             layerEditWindow.Update(gameTime);
-            ToolsWindow.Update(gameTime);
         }
 
         public void Draw(SpriteBatch spriteBatch)
@@ -63,9 +58,8 @@ namespace LevEditor
             {
                 fileWindow.Draw(spriteBatch);
                 assetsWindow.Draw(spriteBatch);
-                tileEditWindow.Draw(spriteBatch);
+                toolsWindow.Draw(spriteBatch);
                 layerEditWindow.Draw(spriteBatch);
-                ToolsWindow.Draw(spriteBatch);
             }
         }
     }
--- LevEditor/src/HUD/Window/ToolsWindow.cs
+++ LevEditor/src/HUD/Window/ToolsWindow.cs
@@ -3,36 +3,29 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LevEditor
 {
-    /* TODO:
-     * -Undo
-     * -Redo
-     * -Paint-grab/smart-grab
-     * -Show/hide grid
-     */
     class ToolsWindow : Window
     {
         public ToolsWindow(Vector2 position, Vector2 dimensions) : base(position, dimensions)
         {
         }
 
-        new public void Load(ContentManager content, string backgroundFilePath)
+        public void Load(ContentManager content, string filePath)
         {
-            base.Load(content, backgroundFilePath);
+            base.Load(content, filePath);
         }
 
-        new public void Update(GameTime gameTime)
+        public void Update(GameTime gameTime)
         {
             base.Update(gameTime);
         }
 
-        new public void Draw(SpriteBatch spriteBatch)
+        public void Draw(SpriteBatch spriteBatch)
         {
             base.Draw(spriteBatch);
         }
