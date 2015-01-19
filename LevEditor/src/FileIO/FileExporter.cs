b66ae5b4ee1b85c0493e59a79811533b9449d56a
Assassin0795 <tonycarmalitano@yahoo.com>
2015-01-19 13:35:20 -0500

    Added skeleton functions.

--- LevEditor/src/FileIO/FileExporter.cs
+++ LevEditor/src/FileIO/FileExporter.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace LevEditor
 {
-    class FileExporter
+    public static class FileExporter
     {
+        public enum IMGFileExtension
+        {
+            PNG,
+            JPG,
+        }
+
+        // Exports to a data file that can be read in to modify the level.
+        public static void ExportCanvas()
+        {
+        }
+
+        // Exports to an image file (NOT modifiable) that can be used for backgrounds.
+        public static void ExportCanvasImg(IMGFileExtension extension)
+        {
+        }
+
+        public static void ImportCanvas()
+        {
+        }
     }
 }
